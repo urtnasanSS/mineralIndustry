f04f540c139c7f4b46605687faa9aedca3cbc743
BACTUSS1111 <56919278+BACTUSS1111@users.noreply.github.com>
2020-06-26 10:07:41 +0800

    modified:   components/HeaderWithNavbar.vue 	components/CommitChart.js 	components/PieChart.js 	components/calculator.vue 	components/foother.vue 	components/leftColoumChart.vue 	components/leftColoumDiscussion.vue 	components/leftColoumNews.vue 	components/rightColoum.vue 	pages/contactUs.vue 	pages/maps.vue 	pages/page2.vue 	pages/page3.vue

--- frontend/components/HeaderWithNavbar.vue
+++ frontend/components/HeaderWithNavbar.vue
@@ -42,23 +42,143 @@
 </template>
 <script>
 </script>
-<style>
-.center{ display:block; margin-left:10% ; margin-right:10% ;padding:0%; }
-.main_menu_area{width: 100%; ; background:#1a3675; min-height:50px; overflow: hidden; }
-.main_menu_area ul{margin:0; padding:0; list-style:none; overflow: hidden;}
-.main_menu_area ul li{float:left; position:relative; overflow: hidden;}
-.main_menu_area ul li a{
-color:#FFF; 
-overflow: hidden;
-display:block; 
-font-family: SegoeUI; 
-font-size: 16px; 
-font-weight: bold; 
-font-stretch: normal; 
-font-style: normal;
-text-decoration: none;
-line-height: 1.33;
-letter-spacing: normal;
-text-align: left; padding:18px 18.7px}
+<style lang="scss" scoped>
+.hero {
+  min-height: 250px;
+  @media screen and (max-width: 1407px) {
+    min-height: 249px;
+  }
+  @media screen and (max-width: 1215px) {
+    min-height: 232px;
+  }
+  @media screen and (max-width: 1023px) {
+    min-height: 188px;
+  }
+}
+.backgroundImage {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  .columns {
+    .column {
+      position: relative;
+      display: flex;
+      justify-content: flex-end;
+      .square1 {
+        position: absolute;
+        border-left: 140px solid transparent;
+        height: 0;
+        width: 100%;
+      }
+      .square2 {
+        position: absolute;
+        border-left: 140px solid transparent;
+        height: 0;
+        width: 45%;
+      }
+    }
+  }
+  // z-index: 0;
+  // position: absolute;
+  // // border: solid 1px red;
+  // height: 200px;
+  // width: 100%;
+  // // top:-250px;
+  // background: url('/img/headerbgcover.png');
+  // background-repeat: no-repeat;
+  // background-position: right;
+  //   background-size: contain;
+}
+.headerLine {
+  min-height: 25px;
+  border-bottom: solid 1px #dcdfe6;
+  line-height: 20px;
+  .container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    .header-line-left {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      li {
+        border-right: solid 1px #dcdfe6;
+        text-align: center;
+        width: 80px;
+      }
+    }
+    .header-line-right {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+    }
+    a {
+      color: white;
+      font-size: 10px;
+      height: 15px;
+      text-transform: uppercase;
+      width: 80px;
+      text-align: center;
+      &:hover {
+        color: #cbdff7;
+      }
+    }
+    img {
+      height: 12px;
+    }
+  }
+}
+.headerLin2222 {
+  border-bottom: solid 1px #dcdfe6;
+  height: 35px;
+  .container {
+    position: relative;
+    .columns {
+      .column {
+        padding: 8px;
+        .headerNav {
+          list-style-type: none;
+          z-index: 2222;
+          margin: 0;
+          padding: 0;
+          // float: right;
+          // margin-top: 5px;
+          // height: 15px !important;
+          padding-right: 10px;
+          li {
+            display: inline-flex;
+            border-right: solid 1px #dcdfe6;
+            padding-right: 8px;
+            padding-top: 7px;
+            a {
+              color: white;
+              font-size: 10px;
+              height: 15px;
+              text-transform: uppercase;
+              &:hover {
+                // font-weight: bold;
+                color: #cbdff7;
+              }
+            }
+            img {
+              // margin-top: 5px;
+              height: 12px;
+              margin-right: 5px;
+            }
+          }
+          li:last-child {
+            border-right: none;
+          }
+        }
+      }
+    }
+  }
+}
+.menuBackground {
+  background-color: #da0717;
+  z-index: 99 !important;
+}
 
 </style>
